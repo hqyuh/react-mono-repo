2da4676a50870670187016be1017b2adcb793312
Huy <ho.huy@hitachids.com>
2024-10-26 13:00:32 +0700

    add eslint

--- apps/teams/src/App.tsx
+++ apps/teams/src/App.tsx
@@ -1,11 +1,20 @@
-import { Banner, H1 } from "@mono/ui";
-import { Button } from "@ui/components/ui/button";
+import { Banner, H1 } from '@mono/ui';
+import { Button } from '@ui/components/ui/button';
+
+type IProps = {
+  instanceName: string;
+};
+
+const t: IProps = {
+  instanceName: 'Teams'
+};
+console.log('🐔 =>  t:', t);
 
 function App() {
   return (
     <>
-      <div className="text-5xl">Teams</div>
-      <Banner instanceName="Teams" />
+      <div className='text-5xl'>Teams</div>
+      <Banner instanceName='Teams' />
       <H1 />
       <Button>CC</Button>
     </>
